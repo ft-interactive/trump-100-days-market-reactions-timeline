2dab2211d4dc1981db338e1ca1d4efbc5e221854
Joanna Kao <joanna.kao@ft.com>
2017-04-21 14:59:34 -0400

    redraw charts with debounce on window resize

--- client/index.js
+++ client/index.js
@@ -1,10 +1,20 @@
+import debounce from 'lodash.debounce';
 import drawLineChart from './components/chart/line';
 
-const charts = document.querySelectorAll('.chart');
+function drawCharts() {
+  const charts = document.querySelectorAll('.chart');
 
-Array.from(charts).forEach((chart) => {
-  const container = chart.querySelector('.chart_container');
-  const indicator = chart.dataset.indicator;
+  Array.from(charts).forEach((chart) => {
+    const container = chart.querySelector('.chart_container');
+    const indicator = chart.dataset.indicator;
 
-  drawLineChart(container, indicator);
+    container.innerHTML = '';
+    drawLineChart(container, indicator);
+  });
+}
+
+drawCharts();
+
+window.addEventListener('resize', () => {
+  debounce(drawCharts(), 100);
 });
